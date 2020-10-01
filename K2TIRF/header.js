957c796585e1564f33b049fb6d27dd9aaa5af3df
Christian Niederauer <niederauer@Amolf.nl>
2020-10-01 12:13:48 +0200

    initial commit

---
+++ K2TIRF/header.js
@@ -0,0 +1,16 @@
+document.write('\
+<h1 class="header">The K2: Multicolor single-molecule TIRF microscope</h1>\
+        <ul>\
+		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
+		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
+		<li class="first"><a href="https://hohlbeinlab.github.io/miCube/index.html" target="_self">miCube project</a></li>\
+				<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
+		<font style="line-height:1.5em;">\
+        <table>\
+		<tr><td>\
+		<a class="buttons github" href="https://github.com/GanzingerLab/K2TIRF" style="padding-top:0px;">View On GitHub</a></td><td>&nbsp;&nbsp;&nbsp;</td><td>\
+		<tr><td colspan="3"><a class="buttons bip" href="https://amolf.nl/research-groups/physics-of-cellular-interactions" style="padding-top:0px; padding-left:60px;" >Physics of Cellular Interactions</a></td></tr>\
+</table>\
+</font>\
+        </ul>\
+');
