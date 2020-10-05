46f096617f8e0c14538bd91a7009a8b7738eab51
Christian Niederauer <niederauer@Amolf.nl>
2020-10-05 19:03:27 +0200

    people page

--- K2TIRF/header.js
+++ K2TIRF/header.js
@@ -3,8 +3,7 @@ document.write('\
         <ul>\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
-		<li class="first"><a href="https://hohlbeinlab.github.io/miCube/index.html" target="_self">miCube project</a></li>\
-			<li class="first"><a href="https://hohlbeinlab.github.io/miCube/index.html" target="_self">miCube project</a></li>\
+		<li class="first"><a href="./people.html" target="_self">People involved</a></li>\
 				<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 		<font style="line-height:1.5em;">\
         <table>\
