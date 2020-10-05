d28d46807b3335a0ef9d0a1c1f8eaf11eb06e85c
Christian Niederauer <niederauer@Amolf.nl>
2020-10-05 19:00:22 +0200

    triggerbox

--- K2TIRF/header.js
+++ K2TIRF/header.js
@@ -4,6 +4,7 @@ document.write('\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
 		<li class="first"><a href="https://hohlbeinlab.github.io/miCube/index.html" target="_self">miCube project</a></li>\
+			<li class="first"><a href="https://hohlbeinlab.github.io/miCube/index.html" target="_self">miCube project</a></li>\
 				<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 		<font style="line-height:1.5em;">\
         <table>\
