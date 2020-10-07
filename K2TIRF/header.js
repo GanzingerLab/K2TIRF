e98970c6590fd6cbc84e92b0230e7ddc09268b8f
Christian Niederauer <niederauer@Amolf.nl>
2020-10-07 12:47:56 +0200

    added about page

--- K2TIRF/header.js
+++ K2TIRF/header.js
@@ -3,7 +3,7 @@ document.write('\
         <ul>\
 		<li class="first"><a href="./index.html" target="_self">Main page</a></li>\
 		<li class="first"><a href="./component_table.html" target="_self">Component overview</a></li>\
-		<li class="first"><a href="./people.html" target="_self">People involved</a></li>\
+		<li class="first"><a href="./about.html" target="_self">About the project</a></li>\
 				<div style="padding-top:10px;"></div><hr style=" margin: 0.1em auto;"><div style="padding-top:10px;"></div>\
 		<font style="line-height:1.5em;">\
         <table>\
